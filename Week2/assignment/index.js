47f763e5cb9b4764ccfc833e00493f17c3df103a
봄 <saebom@Saebomui-MacBookPro.local>
2025-04-21 20:51:06 +0900

    feat: 필터링 기능 추가

--- Week2/assignment/index.js
+++ Week2/assignment/index.js
@@ -1,5 +1,54 @@
 import { todos } from "./data.js";
 
-localStorage.setItem("todos", JSON.stringify(todos));
+if (!localStorage.getItem("todos")) {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
 
-console.log(todos);
+const todoItems = JSON.parse(localStorage.getItem("todos")) || [];
+const table = document.querySelector(".table-body");
+const filterButtons = document.querySelectorAll(".tag-container .button");
+
+function renderTodos(todos) {
+  table.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const tr = document.createElement("tr");
+
+    const tdCheckbox = document.createElement("td");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    tdCheckbox.appendChild(checkbox);
+
+    const tdPriority = document.createElement("td");
+    tdPriority.textContent = todo.priority;
+
+    const tdStatus = document.createElement("td");
+    tdStatus.textContent = todo.completed ? "✅" : "❌";
+
+    const tdTodo = document.createElement("td");
+    tdTodo.textContent = todo.title;
+
+    tr.appendChild(tdCheckbox);
+    tr.appendChild(tdPriority);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdTodo);
+
+    table.appendChild(tr);
+  });
+}
+
+document.querySelector(".all").addEventListener("click", () => {
+  renderTodos(todoItems);
+});
+
+document.querySelector(".finished").addEventListener("click", () => {
+  const filtered = todoItems.filter((todo) => todo.completed);
+  renderTodos(filtered);
+});
+
+document.querySelector(".unfinished").addEventListener("click", () => {
+  const filtered = todoItems.filter((todo) => !todo.completed);
+  renderTodos(filtered);
+});
+
+renderTodos(todoItems);
