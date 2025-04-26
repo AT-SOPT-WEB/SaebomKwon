d4b1de6604210c1927b3a53a4a97dd28f7da2d5a
봄 <saebom@Saebomui-MacBookPro.local>
2025-04-26 16:33:20 +0900

    feat: 3주차 연습

---
+++ Week3/practice/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import style from "./Header.module.css";
+
+const Header = () => {
+  return <h1 className={style.header}>스웹팟 파트원</h1>;
+};
+
+export default Header;
---
+++ Week3/practice/src/components/Search.jsx
@@ -0,0 +1,15 @@
+const Search = ({ search, handleSearchChange, handleSearch }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        value={search}
+        onChange={handleSearchChange}
+        placeholder="검색어를 입력하세요"
+      />
+      <button onClick={handleSearch}>검색</button>
+    </div>
+  );
+};
+
+export default Search;
