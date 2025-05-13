2876272fc73c4ff663db208597bbed1167fb07a1
Saebom <spring180@naver.com>
2025-05-13 17:57:51 +0900

    feat: 로그인 페이지 파일 수정

--- Week4/assignment/src/pages/LoginPage.tsx
+++ Week4/assignment/src/pages/LoginPage.tsx
@@ -1,9 +1,30 @@
-import Form from "@/components/Form";
+import { css } from "@emotion/react";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import LinkItem from "@/components/LinkItem";
+
+import routePath from "@/routes/routePath";
 
 export default function LoginPage() {
   return (
     <>
-      <Form title="로그인" />
+      <h1 css={titleStyle}>로그인</h1>
+      <div css={InputContainer}>
+        <Input type="id" />
+        <Input type="pw" />
+        <Button label="로그인"></Button>
+        <LinkItem to={routePath.SIGNUP}>회원가입</LinkItem>
+      </div>
     </>
   );
 }
+const InputContainer = css`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const titleStyle = css`
+  text-align: center;
+  padding: 2rem;
+`;
